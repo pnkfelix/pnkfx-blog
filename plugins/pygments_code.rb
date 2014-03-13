e83031ecad918dacc79d4d966ef5c55edb8094ac
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-03-13 14:28:52 +0100

    Fix customization to pull in my local copy of pygments.

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -5,7 +5,7 @@ require 'digest/md5'
 PYGMENTS_CACHE_DIR = File.expand_path('../../.pygments-cache', __FILE__)
 FileUtils.mkdir_p(PYGMENTS_CACHE_DIR)
 
-Pygments.start('../custom-pygments/pygments')
+Pygments.start(File.expand_path('../../custom-pygments', __FILE__))
 
 module HighlightCode
   def highlight(str, lang)
